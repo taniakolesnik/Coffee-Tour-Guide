10ece29bc5ccdc802fa0a1a7dc5b1cf0bbc93f4a
Tania Kolesnik <kolesnik@gmail.com>
2018-01-11 22:23:09 +0000

    Start sliding from MainPage. Sliding left and right

--- app/src/main/java/com/example/android/tourguide/MainActivity.java
+++ app/src/main/java/com/example/android/tourguide/MainActivity.java
@@ -1,29 +1,55 @@
 package com.example.android.tourguide;
 
-import android.app.Activity;
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.list_view);
+        setContentView(R.layout.activity_main);
 
-        ArrayList<Organisation> organisations = new ArrayList<Organisation>();
-        organisations.add(new Organisation("Tania", "London", "111111"));
-        organisations.add(new Organisation("Olia", "Essen", "00000"));
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+        viewPager.setAdapter(new SimpleFragmentPagerAdapter(getSupportFragmentManager(), getApplicationContext()));
+        viewPager.setCurrentItem(1);
 
-        ListView listView = (ListView) findViewById(R.id.list);
-        OrganisationListAdapter listAdapter = new OrganisationListAdapter(this, organisations);
-        listView.setAdapter(listAdapter);
 
+    }
+
+    private class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
+
+        public SimpleFragmentPagerAdapter(FragmentManager supportFragmentManager, Context applicationContext) {
+            super(supportFragmentManager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new WestFragment();
+                case 1:
+                    return new MainPageFragment();
+                case 2:
+                    return new EastFragment();
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
     }
 
 }
