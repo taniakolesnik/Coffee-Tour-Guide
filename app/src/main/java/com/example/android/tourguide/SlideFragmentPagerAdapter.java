3db3d856ac40c1b2ee0984e3cc49111f2fb69eff
Tania Kolesnik <kolesnik@gmail.com>
2018-02-24 00:44:28 +0000

    After review changes 3

--- app/src/main/java/com/example/android/tourguide/SlideFragmentPagerAdapter.java
+++ app/src/main/java/com/example/android/tourguide/SlideFragmentPagerAdapter.java
@@ -1,7 +1,6 @@
 package com.example.android.tourguide;
 
 import android.content.Context;
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -12,12 +11,14 @@ public class SlideFragmentPagerAdapter extends FragmentPagerAdapter {
     private static final int POSITION_ONE = 1;
     private static final int POSITION_TWO = 2;
     private static final int POSITION_THREE = 3;
+    private static final int TABS_NUMBER = 4;
+    private Context mContext;
+
 
     public SlideFragmentPagerAdapter(FragmentManager supportFragmentManager, Context applicationContext) {
         super(supportFragmentManager);
+        mContext = applicationContext;
     }
-
-    private String fragmentsTabsName [] = {"West", "South", "North", "East"} ;
 
     @Override
     public Fragment getItem(int position) {
@@ -37,11 +38,22 @@ public class SlideFragmentPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public int getCount() {
-        return fragmentsTabsName.length;
+        return TABS_NUMBER;
     }
 
     @Override
     public CharSequence getPageTitle(int position) {
-        return fragmentsTabsName[position];
+        switch (position) {
+                case POSITION_ZERO:
+                    return mContext.getString(R.string.west_name);
+                case POSITION_ONE:
+                    return mContext.getString(R.string.south_name);
+                case POSITION_TWO:
+                    return mContext.getString(R.string.north_name);
+                case POSITION_THREE:
+                    return mContext.getString(R.string.east_name);
+                default:
+                    return null;
+            }
     }
 }
