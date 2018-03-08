fa66c30ad77fd52138170f5c31f553f78f641f80
Tania Kolesnik <kolesnik@gmail.com>
2018-03-08 20:38:30 +0000

    Tabs name from strings.xml, set names via string array, remove switch and get tabs number from array.lenght

--- app/src/main/java/com/example/android/tourguide/SlideFragmentPagerAdapter.java
+++ app/src/main/java/com/example/android/tourguide/SlideFragmentPagerAdapter.java
@@ -7,17 +7,22 @@ import android.support.v4.app.FragmentPagerAdapter;
 
 public class SlideFragmentPagerAdapter extends FragmentPagerAdapter {
 
+
+    private Context context;
+    private String[] fragmentsTabsName;
     private static final int POSITION_ZERO = 0;
     private static final int POSITION_ONE = 1;
     private static final int POSITION_TWO = 2;
     private static final int POSITION_THREE = 3;
     private static final int TABS_NUMBER = 4;
-    private Context mContext;
-
 
     public SlideFragmentPagerAdapter(FragmentManager supportFragmentManager, Context applicationContext) {
         super(supportFragmentManager);
-        mContext = applicationContext;
+        context = applicationContext;
+        fragmentsTabsName = new String[] {context.getResources().getString(R.string.west_name),
+                context.getResources().getString(R.string.south_name),
+                context.getResources().getString(R.string.north_name),
+                context.getResources().getString(R.string.east_name)} ;
     }
 
     @Override
@@ -38,22 +43,11 @@ public class SlideFragmentPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public int getCount() {
-        return TABS_NUMBER;
+        return fragmentsTabsName.length;
     }
 
     @Override
     public CharSequence getPageTitle(int position) {
-        switch (position) {
-                case POSITION_ZERO:
-                    return mContext.getString(R.string.west_name);
-                case POSITION_ONE:
-                    return mContext.getString(R.string.south_name);
-                case POSITION_TWO:
-                    return mContext.getString(R.string.north_name);
-                case POSITION_THREE:
-                    return mContext.getString(R.string.east_name);
-                default:
-                    return null;
-            }
+        return fragmentsTabsName[position];
     }
 }
